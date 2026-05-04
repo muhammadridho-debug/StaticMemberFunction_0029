e3daa5ba3254a84b8120131962db6c58ca0a60d0
muhammadridho-debug <idomuhammad11@gmail.com>
2026-05-04 12:46:53 +0700

    deklarasi prototypes untuk constructor overloading dan method cetak

--- constructorbanyak.cpp
+++ constructorbanyak.cpp
@@ -7,6 +7,11 @@ private:
     int nim;
     string nama;
 public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
 
 int main() {
