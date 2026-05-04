f8858a48c186477fafab38ec9475c0257062f591
muhammadridho-debug <idomuhammad11@gmail.com>
2026-05-04 12:51:53 +0700

    instansiasi objek mhs1, mhs2, mhs3 dan pemanggilan method cetak

--- constructorbanyak.cpp
+++ constructorbanyak.cpp
@@ -36,5 +36,13 @@ void mahasiswa::cetak() {
 }
 
 int main() {
+    mahasiswa mhs1(102030);                   // hanya nim       
+    mahasiswa mhs2("Andi");                   // hanya nama  
+    mahasiswa mhs3(010203, "Santi");          // hanya nim dan nama
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
     return 0;
 }
